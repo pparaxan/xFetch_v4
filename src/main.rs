508f4ce6a0687aea1f53f317f505a1e58c204279
xan <xxdr@duck.com>
2024-12-29 12:03:44 -0500

    refactor: remove init
    Sorry @WilliamAnimate but it seems abit off

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::io::{self, Write};
 use tokio::{join, task::spawn};
 
 pub mod distro;
-pub mod init;
 #[macro_use]
 pub mod macros;
 pub mod packages;
@@ -14,19 +13,17 @@ pub mod uptime;
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let distro_thread = spawn(async { distro::get_distro_name().await });
-    let init_thread = spawn(async { init::get_init_system().await });
     let name_thread = spawn(async { get_env_var!("USER") });
     let packages_thread = spawn(async { packages::get_current_packages() });
     let shell_thread = spawn(async { shell::get_current_shell().await });
     let uptime_thread = spawn(async { uptime::get_uptime().await });
 
-    let (usr, distro, shell, pkg, uptime, init) = join!(
+    let (usr, distro, shell, pkg, uptime) = join!(
         name_thread,
         distro_thread,
         shell_thread,
         packages_thread,
         uptime_thread,
-        init_thread,
     );
 
     let distro = distro.unwrap();
@@ -34,7 +31,6 @@ async fn main() -> io::Result<()> {
     let shell = shell.unwrap();
     let uptime = uptime.unwrap();
     let usr = usr.unwrap();
-    let init = init.unwrap();
     let mut handle = io::stdout().lock(); // Lock stdout for slightly faster writing
 
     writeln!(handle, "\x1B[0;31m\x1B[1mx\x1B[0;36mFetch\x1B[0m - {}", usr);
@@ -42,7 +38,6 @@ async fn main() -> io::Result<()> {
     writeln_to_handle_if_not_empty!(handle, "PKGs", pkg);
     writeln_to_handle_if_not_empty!(handle, "Uptime", uptime);
     writeln_to_handle_if_not_empty!(handle, "Distro", distro);
-    writeln_to_handle_if_not_empty!(handle, "Init", init);
 
     drop(handle);
     Ok(())
