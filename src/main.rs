324c20e3fb5e4aebaa29cbffbf98f628d35278e7
xanners <xxdr@duck.com>
2025-01-21 18:41:40 -0500

    refactor: remove user & host name comments

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,16 @@ pub mod macros;
 pub mod packages;
 pub mod shell;
 pub mod uptime;
-// pub mod hostname;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let distro_thread = spawn(async { distro::get_distro_name().await });
-    // let username_thread = spawn(async { get_env_var!("USER") });
-    // let hostname_thread = spawn(async { hostname::get_hostname().await });
     let packages_thread = spawn(async { packages::get_current_packages().await });
     let shell_thread = spawn(async { shell::get_current_shell().await });
     let uptime_thread = spawn(async { uptime::get_uptime().await });
     let terminal_thread = spawn(async { get_env_var!("TERM") });
 
-    // let (username, hostname, distro, shell, pkg, uptime, terminal) = join!(
     let (distro, shell, pkg, uptime, terminal) = join!(
-        // username_thread,
-        // hostname_thread,
         distro_thread,
         shell_thread,
         packages_thread,
@@ -36,8 +30,6 @@ async fn main() -> io::Result<()> {
     let pkg = pkg.unwrap();
     let shell = shell.unwrap();
     let uptime = uptime.unwrap();
-    // let username = username.unwrap();
-    // let hostname = hostname.unwrap();
     let terminal = terminal.unwrap();
     let mut handle = io::stdout().lock(); // Lock stdout for slightly faster writing
 
@@ -46,7 +38,6 @@ async fn main() -> io::Result<()> {
     writeln_to_handle_if_not_empty!(handle, "\x1B[1;33m > ~~ < \x1B[0m  \x1B[0;31m", terminal); // trol lmao (Linux what are you doing to me?); pacman pkgs only, for now.**
     writeln_to_handle_if_not_empty!(handle, "\x1B[1;33m/__/\\_ \\\x1B[0m  \x1B[0;31m󰏗", pkg);
     writeln_to_handle_if_not_empty!(handle, "\x1B[1;33m      \\/\x1B[0m  \x1B[0;31m", uptime);
-    // writeln_to_handle_if_not_empty!(handle, "          \x1B[0;31m󱩛", username, "@", hostname);
 
     drop(handle);
     Ok(())
