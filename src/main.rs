186ba9836ed8a259deb95fd1f19c543cda4bcaf2
xanners <xxdr@duck.com>
2025-01-21 18:23:34 -0500

    refactor: ascii and stuff
    that old one @WilliamAnimate told me to keep was kinda goofy so I'm not gonna use it. (still has the `~~` tho lmao)

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,22 @@ pub mod macros;
 pub mod packages;
 pub mod shell;
 pub mod uptime;
-pub mod hostname;
+// pub mod hostname;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let distro_thread = spawn(async { distro::get_distro_name().await });
-    let username_thread = spawn(async { get_env_var!("USER") });
-    let hostname_thread = spawn(async { hostname::get_hostname().await });
+    // let username_thread = spawn(async { get_env_var!("USER") });
+    // let hostname_thread = spawn(async { hostname::get_hostname().await });
     let packages_thread = spawn(async { packages::get_current_packages().await });
     let shell_thread = spawn(async { shell::get_current_shell().await });
     let uptime_thread = spawn(async { uptime::get_uptime().await });
     let terminal_thread = spawn(async { get_env_var!("TERM") });
 
-    let (username, hostname, distro, shell, pkg, uptime, terminal) = join!(
-        username_thread,
-        hostname_thread,
+    // let (username, hostname, distro, shell, pkg, uptime, terminal) = join!(
+    let (distro, shell, pkg, uptime, terminal) = join!(
+        // username_thread,
+        // hostname_thread,
         distro_thread,
         shell_thread,
         packages_thread,
@@ -35,20 +36,17 @@ async fn main() -> io::Result<()> {
     let pkg = pkg.unwrap();
     let shell = shell.unwrap();
     let uptime = uptime.unwrap();
-    let username = username.unwrap();
-    let hostname = hostname.unwrap();
+    // let username = username.unwrap();
+    // let hostname = hostname.unwrap();
     let terminal = terminal.unwrap();
     let mut handle = io::stdout().lock(); // Lock stdout for slightly faster writing
 
-    // writeln!(handle, "\x1B[0mxx\x1B[0;3mx\x1B[0;1mFetch\x1B[0m");
-    // writeln!(handle, "This is my \x1B[0;3mx\x1B[0;1mFetch\x1B[0m btw");
-    // writeln!(handle, "'I use \x1B[0;3mxfetch\x1B[0m btw.'");
-    writeln_to_handle_if_not_empty!(handle, "", distro);
-    writeln_to_handle_if_not_empty!(handle, "󱩛", username, "@", hostname);
-    writeln_to_handle_if_not_empty!(handle, "", shell);
-    writeln_to_handle_if_not_empty!(handle, "󰏗", pkg); // pacman pkgs only
-    writeln_to_handle_if_not_empty!(handle, "", uptime);
-    writeln_to_handle_if_not_empty!(handle, "", terminal);
+    writeln_to_handle_if_not_empty!(handle, "\x1B[0;33m___  ___\x1B[0m  \x1B[0;31m", shell);
+    writeln_to_handle_if_not_empty!(handle, "\x1B[1;33m\\  \\/  /\x1B[0m  \x1B[0;31m", distro);
+    writeln_to_handle_if_not_empty!(handle, "\x1B[1;33m > ~~ < \x1B[0m  \x1B[0;31m", terminal); // trol lmao (Linux what are you doing to me?); pacman pkgs only, for now.**
+    writeln_to_handle_if_not_empty!(handle, "\x1B[1;33m/__/\\_ \\\x1B[0m  \x1B[0;31m󰏗", pkg);
+    writeln_to_handle_if_not_empty!(handle, "\x1B[1;33m      \\/\x1B[0m  \x1B[0;31m", uptime);
+    // writeln_to_handle_if_not_empty!(handle, "          \x1B[0;31m󱩛", username, "@", hostname);
 
     drop(handle);
     Ok(())
